2f1fee5e1e51ec2dce49b0074b232a7a93440811
Peter Duong <ptduong90@gmail.com>
2021-04-13 15:53:37 -0700

    added function
    completed validation functions:
    -name
    -email
    -credit card
    -activity
    -form submit validation

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-//global variables
-let form = document.querySelector('form');
-
-
 //Name field default focus
 function defaultFocus() {
     let nameFocus = document.getElementById('name');
     nameFocus.focus();
-
 }
 defaultFocus();
-
 
 //Job select and text field visibility
 function selectJob() {
@@ -56,15 +50,15 @@ shirtDesign();
 
 //Activity's cost will be added at the bottom when each checkbox is checked. Cost will be deducted when checkbox 
 //is unchecked.
+let activitySelect = document.getElementById('activities-box');
+let activityError = document.getElementById('activities-hint');
+let activityField = document.getElementById('activities');
+
 function activityFieldset() {
-    let activityField = document.getElementById('activities');
+    // let activityField = document.getElementById('activities');
     let priceTotal = document.getElementById('activities-cost');
-    let activitySelect = document.getElementById('activities-box');
-    let activityError = document.getElementById('activities-hint');
-    
     
     activityField.addEventListener('change', ()=> {
-        //let activitySelect = document.getElementById('activities-box');
         let mainCon = activitySelect.firstElementChild;
         let jsWorkshop = mainCon.nextElementSibling;
         let nodeWorkshop = jsWorkshop.nextElementSibling;
@@ -79,9 +73,6 @@ function activityFieldset() {
         let buildWorkshopPrice = buildWorkshop.firstElementChild.getAttribute('data-cost');
         let npmWorkshopPrice = npmWorkshop.firstElementChild.getAttribute('data-cost');
         let expressWorkshopPrice = expressWorkshop.firstElementChild.getAttribute('data-cost');
-
-        
-       
         
         function runningTotal() {
             let total = 0;
@@ -150,19 +141,12 @@ function activityFieldset() {
             }
             
             //validates if at least one activity is checked
-            function activityValidator() {
-    
-                if (total === 0) {
-                            
-                    activityError.style.display = 'inherit';
-                } else {
-                    activityError.style.display = 'none';
-                }
-                
+            if (total === 0) {             
+                activityError.style.display = 'inherit';
+            } else {
+                activityError.style.display = 'none';
             }
-            activityValidator();
-            
-            
+                
             priceTotal.textContent = `Total: $${total}`;
             return total;
         }
@@ -174,9 +158,12 @@ activityFieldset();
 //Payment section. Default payment needs to be credit card
 //Searched google for .defaultSelected. Source: 
 //https://stackoverflow.com/questions/4134070/how-to-set-the-default-option-for-select-tag-and-how-to-get-the-index-of-the-sel
+
+let paymentSelect = document.getElementById('payment');
+let creditCardBox = document.getElementById('credit-card');
+
 function paymentMethod() {
-    let paymentSelect = document.getElementById('payment');
-    let creditCardBox = document.getElementById('credit-card');
+    
     paymentSelect.options[1].defaultSelected = true;
 
     //payment method select listens for change to paypal or bitcoin to hide credit card box.
@@ -185,46 +172,226 @@ function paymentMethod() {
        if (paymentSelect.selectedIndex === 2 || paymentSelect.selectedIndex === 3) {
            creditCardBox.style.display = 'none';
        } else if (paymentSelect.selectedIndex === 1) {
-           creditCardBox.style.display = '';
-       }       
+           creditCardBox.style.display = 'inherit';
+       }   
     });
 }
 paymentMethod();
 
 //Input validation for name, email and activities. Referenced and used code for showOrHideTip and createListener from Team Treehouse regex workspace activity.
+let form = document.querySelector('form');
 let usernameInput = document.getElementById('name');
 let emailInput = document.getElementById('email');
-//let allActivities = document.querySelectorAll('input[type="checkbox"]');
+let ccNumBoxInput = document.getElementById('cc-num');
+let zipBoxInput = document.getElementById('zip');
+let cvvBoxInput = document.getElementById('cvv');
+let validName = /^([a-zA-Z])+\s?([a-zA-Z]*)+\s?$/i;
+let validEmail = /^([a-z0-9]*)+\@([a-z])+\.([a-z])+$/i;
+let validccNumber = /^(\d{13})\d?\d?\d?$/;
+let validZip = /^(\d{5})$/;
+let validCvv = /^(\d{3})$/;
 
+//Name validator
+function nameValidator() {
+    usernameInput.addEventListener('input', ()=> {
 
-function nameValidator(username) {  
-    return /^([a-zA-Z]*)+\s?([a-zA-Z]*)+\s?$/.test(username);
+        if (usernameInput.value === "") {
+            usernameInput.nextElementSibling.style.display = 'inherit';
+        }
+        if (validName.test(usernameInput.value)) {
+            usernameInput.nextElementSibling.style.display = 'none';
+            usernameInput.parentElement.className = 'valid';
+        } else if (validName.test(usernameInput.value) === false) {
+            usernameInput.parentElement.className = 'not-valid';
+            usernameInput.nextElementSibling.textContent = 'Please enter a valid name';
+            usernameInput.nextElementSibling.style.display = 'inherit';
+        }
+    })
 }
+nameValidator();
 
-function emailValidator(email) {
-    return /^[a-z0-9]+\@[a-z]+\.[a-z]+$/i.test(email);
+//Email validator
+function emailValidator() {
+    emailInput.addEventListener('input', ()=> {
+        if (emailInput.value === "") {
+            emailInput.nextElementSibling.style.display = 'inherit';
+        }
+        if (validEmail.test(emailInput.value)) {
+            emailInput.nextElementSibling.style.display = 'none';
+            emailInput.parentElement.className = 'valid';
+        } else if (validEmail.test(emailInput.value) === false) {
+            emailInput.parentElement.className = 'not-valid';
+            emailInput.nextElementSibling.style.display = 'inherit';
+        }
+    })
 }
+emailValidator();
+
+//Credit Card number validator
+
+function ccValidator() {
+    ccNumBoxInput.addEventListener('input', ()=> {
+        if (ccNumBoxInput.value === "") {
+            ccNumBoxInput.nextElementSibling.style.display = 'inherit';
+        }
+        if (validccNumber.test(ccNumBoxInput.value)) {
+            ccNumBoxInput.nextElementSibling.style.display = 'none';
+            ccNumBoxInput.parentElement.className = 'valid';
+        } else if (validccNumber.test(ccNumBoxInput.value) === false) {
+            ccNumBoxInput.parentElement.className = 'not-valid';
+            ccNumBoxInput.nextElementSibling.style.display = 'inherit';
+        }
+    })
+}
+ccValidator();
 
 
+//Zipcode validator
+function zipValidator() {
+    zipBoxInput.addEventListener('input', ()=> {
+        if (zipBoxInput.value === "") {
+            zipBoxInput.nextElementSibling.style.display = 'inherit';
+        }
+        if (validZip.test(zipBoxInput.value)) {
+            zipBoxInput.nextElementSibling.style.display = 'none';
+            zipBoxInput.parentElement.className = 'valid';
+        } else if (validZip.test(zipBoxInput.value) === false) {
+            zipBoxInput.parentElement.className = 'not-valid';
+            zipBoxInput.nextElementSibling.style.display = 'inherit';
+        }
+    })
+}
+zipValidator();
 
-function showOrHideTip (show, element) {
-    if (show) {
-        element.style.display = "inherit";
+
+//Cvv validator
+function cvvValidator() {
+    cvvBoxInput.addEventListener('input', ()=> {
+        if (cvvBoxInput.value === "") {
+            cvvBoxInput.nextElementSibling.style.display = 'inherit';
+        }
+        if (validCvv.test(cvvBoxInput.value)) {
+            cvvBoxInput.nextElementSibling.style.display = 'none';
+            cvvBoxInput.parentElement.className = 'valid';
+        } else if (validCvv.test(cvvBoxInput.value) === false) {
+            cvvBoxInput.parentElement.className = 'not-valid';
+            cvvBoxInput.nextElementSibling.style.display = 'inherit';
+        }
+    })  
+}
+cvvValidator();
+
+
+function activityValidator() {
+    let activityTotal = document.getElementById('activities-cost');
+    let activityErr = document.getElementById('activities-hint');
+    if (activityTotal.textContent === 'Total: $0') {  
+        activityErr.style.display = 'inherit';
+        return false;
     } else {
-        element.style.display = "none";
+        activityErr.style.display = 'none';
+        return true;
+    }  
+}
+
+//Accessability
+function inputFocus () {
+    let activityFocus = document.querySelectorAll('input[type="checkbox"]');
+    for (let i = 0; i < activityFocus.length; i++ ) {
+        activityFocus[i].addEventListener('focus', ()=> {
+            activityFocus[i].parentElement.className = 'focus';
+        })
+        activityFocus[i].addEventListener('blur', ()=> {
+            activityFocus[i].parentElement.classList.remove('focus');
+        })
     }
 }
+inputFocus();
 
-function createListener(validator) {
-    return e => {
-        let text = e.target.value;
-        let valid = validator(text);
-        let showTip = text !== "" && !valid;
-        let toolTip = e.target.nextElementSibling;
-        showOrHideTip(showTip, toolTip);
-    };
-}
-createListener();
 
-usernameInput.addEventListener('input', createListener(nameValidator));
-emailInput.addEventListener('input', createListener(emailValidator));
+form.addEventListener('submit', (e)=> {
+    
+    
+    //username submit validation
+    if (usernameInput.value === "") {
+        usernameInput.nextElementSibling.style.display = 'inherit';
+        e.preventDefault();
+    }
+    if (validName.test(usernameInput.value)) {
+        usernameInput.nextElementSibling.style.display = 'none';
+        usernameInput.parentElement.className = 'valid';
+    } else if (validName.test(usernameInput.value) === false) {
+        usernameInput.parentElement.className = 'not-valid';
+        usernameInput.nextElementSibling.textContent = 'Please enter a valid name';
+        usernameInput.nextElementSibling.style.display = 'inherit';
+        e.preventDefault();
+    }
+    
+
+    //email input submit validation
+    if (emailInput.value === "") {
+        emailInput.nextElementSibling.style.display = 'inherit';
+        e.preventDefault();
+    }
+    if (validEmail.test(emailInput.value)) {
+        emailInput.nextElementSibling.style.display = 'none';
+        emailInput.parentElement.className = 'valid';
+    } else if (validEmail.test(emailInput.value) === false) {
+        emailInput.parentElement.className = 'not-valid';
+        emailInput.nextElementSibling.style.display = 'inherit';
+        e.preventDefault();
+    }
+
+    //credit card number submit validation if credit card was selected 
+    if (paymentSelect.selectedIndex === 1) {
+        //cc number
+        if (ccNumBoxInput.value === "") {
+            ccNumBoxInput.nextElementSibling.style.display = 'inherit';
+            e.preventDefault();
+        }
+        if (validccNumber.test(ccNumBoxInput.value)) {
+            ccNumBoxInput.nextElementSibling.style.display = 'none';
+            ccNumBoxInput.parentElement.className = 'valid';
+        } else if (validccNumber.test(ccNumBoxInput.value) === false) {
+            ccNumBoxInput.parentElement.className = 'not-valid';
+            ccNumBoxInput.nextElementSibling.style.display = 'inherit';
+            e.preventDefault();
+        }
+
+        //zip
+        if (zipBoxInput.value === "") {
+            zipBoxInput.nextElementSibling.style.display = 'inherit';
+            e.preventDefault();
+        }
+        if (validZip.test(zipBoxInput.value)) {
+            zipBoxInput.nextElementSibling.style.display = 'none';
+            zipBoxInput.parentElement.className = 'valid';
+        } else if (validZip.test(zipBoxInput.value) === false) {
+            zipBoxInput.parentElement.className = 'not-valid';
+            zipBoxInput.nextElementSibling.style.display = 'inherit';
+            e.preventDefault();
+        }
+
+        //cvv
+        if (cvvBoxInput.value === "") {
+            cvvBoxInput.nextElementSibling.style.display = 'inherit';
+            e.preventDefault();
+        }
+        if (validCvv.test(cvvBoxInput.value)) {
+            cvvBoxInput.nextElementSibling.style.display = 'none';
+            cvvBoxInput.parentElement.className = 'valid';
+        } else if (validCvv.test(cvvBoxInput.value) === false) {
+            cvvBoxInput.parentElement.className = 'not-valid';
+            cvvBoxInput.nextElementSibling.style.display = 'inherit';
+            e.preventDefault();
+        }
+       
+    }   
+
+    if (activityValidator()) {
+        activityField.className = 'valid';
+    } else {
+        activityField.className = 'not-valid';
+        e.preventDefault();
+    }
+});
